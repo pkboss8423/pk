1e99d9d6bd32ce90199fd7c57259c5f9488916fb
pkboss8423 <42733259+pkboss8423@users.noreply.github.com>
2021-01-18 23:07:32 +0530

    pushed

---
+++ Prg.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+class prg{
+public static void main(String args[]){
+try{
+
+Class.forName("oracle.jdbc.driver.OracleDriver");
+
+
+Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","javanetwork","tiger");
+
+Statement stmt=con.createStatement();
+String sql1=("insert into Books values(1432,'DBMS','Navathe',7,'Wisley')");
+String sql2=("update Books set edition=10 where Bid=1432");
+String sql3=("Delete from Books where Bid=12345");
+String sql=("select * from Books");
+
+ResultSet rs2=stmt.executeQuery(sql1);
+ResultSet rs3=stmt.executeQuery(sql2);
+ResultSet rs4=stmt.executeQuery(sql3);
+ResultSet rs=stmt.executeQuery(sql);
+
+
+while(rs.next())
+{
+System.out.println(rs.getInt(1)+ " " +rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5) );
+
+}
+con.close();
+
+}catch(Exception e)
+{
+
+System.out.println(e);
+}
+
+}
+}
